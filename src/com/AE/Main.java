57cc9de2e67e8e1af7496473a406aa2a65692cdd
Angel Rodriguez <arrmixer@gmail.com>
2017-04-04 19:35:37 -0400

    Initial commit

---
+++ src/com/AE/Main.java
@@ -0,0 +1,57 @@
+package com.AE;
+
+public class Main {
+
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+	// write your code here
+        new Thread1().start();
+        new Thread2().start();
+
+
+
+    }
+
+    private static class Thread1 extends Thread {
+        public void run(){
+            System.out.println();
+            synchronized (lock1){
+                System.out.println("Thread 1: Has lock1");
+                try{
+                    Thread.sleep(100);
+                }catch (InterruptedException e){
+
+                }
+                System.out.println("Thread 1: Waiting for lock 2");
+                synchronized (lock2){
+                    System.out.println("Thread 1: Has lock1 and lock2");
+                }
+                System.out.println("Tread 1: Released lock2");
+            }
+            System.out.println("Thread 1: Released lock1. Exiting....");
+        }
+    }
+
+    private static class Thread2 extends Thread {
+        public void run() {
+            synchronized (lock1) {
+                System.out.println("Thread 2 has lock1");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+
+                }
+                System.out.println("Thread 2: Waiting for lock2");
+                synchronized (lock2) {
+                    System.out.println("Thread 2: Has lock1 and lock2");
+                }
+                System.out.println("Thread 2: released lock2");
+            }
+            System.out.println("Thread 2: Released lock1. Exiting....");
+        }
+    }
+}
+
+
